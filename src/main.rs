d06113ae21783658e899eca26211781f1466baa5
udayj <jhunjhunwala.uday@gmail.com>
2025-02-26 11:58:44 +0000

    storage addresses, reverse direction simulation

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ use csv::Writer;
 use serde::{Deserialize, Serialize};
 use starknet_core::types::FieldElement;
 use std::{fs::File, io::Write, str::FromStr};
+use avnu_starknet::{storage_key, storage_mapping_key};
 
 #[derive(Debug, Deserialize)]
 struct SimulationInput {
@@ -89,8 +90,8 @@ async fn simulate_transaction(
             )?, // exchange address
             FieldElement::from_str("0xe8d4a51000")?, // percentage
             FieldElement::from_str("0x6")?, // no. of additional params
-            FieldElement::from_str(&input.token_to)?, // token 0 of pool
-            FieldElement::from_str(&input.token_from)?, // token 1 of pool
+            FieldElement::from_str("0x02655c0a29305847e0d9ca1ae3045ffb8a20348d8d3a5afc71ed867a6152f227")?, // token 0 of pool
+            FieldElement::from_str("0x044d6289f59bf28e2d9c1924e0f6753039932b80e404d01c54ab65e0411f0548")?, // token 1 of pool
             FieldElement::from_str("0x0")?, // fee
             FieldElement::from_str("0x80")?, // tick spacing
             FieldElement::from_str(
@@ -106,6 +107,7 @@ async fn simulate_transaction(
         ))
         .await
         .unwrap();
+    //println!("Trace:{:#?}", result.transaction_trace);
     Ok(SimulationResult {
         token_from: input.token_from.clone(),
         token_to: input.token_to.clone(),
@@ -135,11 +137,11 @@ async fn simulate_transaction_v3(
         resource_bounds: ResourceBoundsMapping {
             l1_gas: ResourceBounds {
                 max_amount: 30000,
-                max_price_per_unit: 47519263922480,
+                max_price_per_unit: 589339005056672,
             },
             l2_gas: ResourceBounds {
                 max_amount: 30000,
-                max_price_per_unit: 47519263922480,
+                max_price_per_unit: 589339005056672,
             },
         },
         tip: 0,
@@ -194,6 +196,7 @@ async fn simulate_transaction_v3(
         ))
         .await
         .unwrap();
+    //println!("Trace:{:#?}", result.transaction_trace);
     Ok(SimulationResult {
         token_from: input.token_from.clone(),
         token_to: input.token_to.clone(),
@@ -208,8 +211,40 @@ async fn simulate_transaction_v3(
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+    /*
+    let token0 = FieldElement::from_hex_be("0x02655c0a29305847e0d9ca1ae3045ffb8a20348d8d3a5afc71ed867a6152f227").unwrap();
+    let token1 = FieldElement::from_hex_be("0x044d6289f59bf28e2d9c1924e0f6753039932b80e404d01c54ab65e0411f0548").unwrap();
+    let fee = FieldElement::from_hex_be("0x0").unwrap();
+    let tick_spacing = FieldElement::from_hex_be("0x80").unwrap();
+    let extension = FieldElement::from_hex_be("0x65e8885b13c84318f43fe77280b842269b6feb6a66947f22fadc70963a14771").unwrap();
+
+    
+    let storage_address = storage_mapping_key("pools", &[token0, token1, fee, tick_spacing, extension]);
+    println!("pools in router:{:?}", storage_address);
+    let storage_address = storage_mapping_key("pools", &[token0, token1]);
+    println!("pools in limit order extension:{:?}", storage_address);
+    let next_tick = FieldElement::from_hex_be("0x80").unwrap();
+    let storage_address = storage_mapping_key("initialized_ticks_crossed_last_crossing", &[token0, token1,next_tick]);
+    println!("initialized_ticks_crossed_last_crossing in limit order extension:{:?}", storage_address);
+
+    let owner = FieldElement::from_hex_be("0x065e8885b13c84318f43fe77280b842269b6feb6a66947f22fadc70963a14771").unwrap();
+    let amount = FieldElement::from_hex_be("0x40").unwrap();
+    let storage_address = storage_mapping_key("saved_balances", &[owner, token1, FieldElement::ZERO, amount]);
+    println!("saved_balances in router:{:?}", storage_address);
+
+    let lower_tick = FieldElement::from_hex_be("0x0").unwrap();
+    let upper_tick = FieldElement::from_hex_be("0x80").unwrap();
+    let sign = FieldElement::from_hex_be("0x0").unwrap();
+    let storage_address = storage_mapping_key("ticks", &[token0, token1, fee, tick_spacing, extension, lower_tick, sign]);
+    println!("saved lower in router:{:?}", storage_address);
+
+    let storage_address = storage_mapping_key("ticks", &[token0, token1, fee, tick_spacing, extension, upper_tick, sign]);
+    println!("upper tick in router:{:?}", storage_address);
+
+    return Ok(());*/
     // Read simulation inputs from JSON file
-    let file = File::open("simulation_inputs.json")?;
+    let file = File::open("simulation_inputs_rev.json")?;
     let inputs: Vec<SimulationInput> = serde_json::from_reader(file)?;
 
     // Set account address
@@ -218,7 +253,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     )?;
 
     // Create CSV writer for results
-    let mut writer = Writer::from_path("simulation_results_v3.csv")?;
+    let mut writer = Writer::from_path("simulation_results_rev.csv")?;
     writer.write_record([
         "token_from",
         "token_to",
@@ -233,7 +268,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let nonce = get_nonce(&account_address).await?;
     // Process each simulation input
     for input in inputs {
-        match simulate_transaction_v3(&input, &account_address, &nonce).await {
+        if(input.token_from_low!="0x60b") {
+            //continue;
+        }
+        match simulate_transaction(&input, &account_address, &nonce).await {
             Ok(result) => {
                 println!("Simulation Result:");
                 println!("Token From: {}", result.token_from);
